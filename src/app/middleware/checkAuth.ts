732bd1ddcd40195deb34a67776a6cb94bcb6a4bd
mohammaduzzal <aalp72142@gmail.com>
2025-07-24 21:34:19 +0600

    completed part-2

---
+++ src/app/middleware/checkAuth.ts
@@ -0,0 +1,30 @@
+import  httpStatus  from 'http-status-codes';
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errorHelpers/AppError";
+import { verifyToken } from '../utils/jwt';
+import { envVars } from '../config/env';
+import { JwtPayload } from 'jsonwebtoken';
+
+export const checkAuth = (...authRoles :string[]) =>async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+
+        const accessToken = req.headers.authorization 
+        if(!accessToken){
+            throw new AppError(httpStatus.BAD_REQUEST,"no token received")
+        }
+
+        const verifiedToken = verifyToken(accessToken,envVars.JWT_ACCESS_SECRET) as JwtPayload
+
+        if(!authRoles.includes(verifiedToken.role)){
+            throw new AppError(httpStatus.BAD_REQUEST, "you are not permitted to view this route")
+        }
+
+        req.user = verifiedToken
+
+        next()
+        
+    } catch (error) {
+        next(error)
+        
+    }
+}
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,37 @@
-import { IUser } from "./user.interface"
-import { User } from "./user.model"
+import  httpStatus  from 'http-status-codes';
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser, Role } from "./user.interface";
+import { User } from "./user.model";
+import bcryptjs from "bcryptjs";
+import { envVars } from '../../config/env';
+import { JwtPayload } from 'jsonwebtoken';
+
 
 const createUser = async(payload : Partial<IUser>)=>{
-    const {name,email} = payload;
-      const user = await User.create({name,email});
+    const {email,password, ...rest} = payload;
+
+    const isUserExist = await User.findOne({email})
+
+    if(isUserExist){
+        throw new AppError(httpStatus.BAD_REQUEST, "user already exist")
+    }
+
+    const hashPassword = await bcryptjs.hash(password as string, Number(envVars.BCRYPTJS_SALT_ROUND))
+
+    
+
+    const authProvider : IAuthProvider = {
+        provider : "credentials",
+        providerId : email as string
+    }
+
+
+      const user = await User.create({
+        email,
+        password : hashPassword,
+        auths :[authProvider],
+        ...rest
+    });
       return user;
 
 }
@@ -19,7 +47,46 @@ const getAllUser = async()=>{
     };
 }
 
+const updateUser = async(userId :string,payload : Partial<IUser> , decodedToken : JwtPayload) =>{
+
+    const isUserExist = await User.findById(userId)
+    if(!isUserExist){
+        throw new AppError(httpStatus.NOT_FOUND,"user not found")
+    }
+
+
+    if(payload.role){
+
+        if(decodedToken.role === Role.GUIDE || decodedToken.role === Role.USER){
+            throw new AppError(httpStatus.FORBIDDEN, "you are not authorized")
+        }
+
+        if(payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN){
+            throw new AppError(httpStatus.FORBIDDEN, "you are not authorized")
+        }
+
+
+    }
+
+    if(payload.isActive || payload.isDelete || payload.isVerified){
+         if(decodedToken.role === Role.GUIDE || decodedToken.role === Role.USER){
+            throw new AppError(httpStatus.FORBIDDEN, "you are not authorized")
+        }
+    }
+
+
+    if(payload.password){
+        payload.password = await bcryptjs.hash(payload.password, Number(envVars.BCRYPTJS_SALT_ROUND))
+    }
+
+
+    const newUpdatedUser = await User.findByIdAndUpdate(userId,payload,{new:true,runValidators:true})
+
+    return newUpdatedUser
+}
+
 export const UserServices = {
     createUser,
     getAllUser,
+    updateUser
 }
