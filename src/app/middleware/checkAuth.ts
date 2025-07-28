0ee4dccc36351ac3ff69af553af87e85f38545bd
mohammaduzzal <aalp72142@gmail.com>
2025-07-28 08:59:17 +0600

    completed part-3

--- src/app/middleware/checkAuth.ts
+++ src/app/middleware/checkAuth.ts
@@ -1,30 +1,48 @@
-import  httpStatus  from 'http-status-codes';
+import httpStatus from 'http-status-codes';
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errorHelpers/AppError";
 import { verifyToken } from '../utils/jwt';
 import { envVars } from '../config/env';
 import { JwtPayload } from 'jsonwebtoken';
+import { User } from '../modules/user/user.model';
+import { IsActive } from '../modules/user/user.interface';
 
-export const checkAuth = (...authRoles :string[]) =>async(req:Request,res:Response,next:NextFunction)=>{
+export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
     try {
 
-        const accessToken = req.headers.authorization 
-        if(!accessToken){
-            throw new AppError(httpStatus.BAD_REQUEST,"no token received")
+        const accessToken = req.headers.authorization
+        if (!accessToken) {
+            throw new AppError(httpStatus.BAD_REQUEST, "no token received")
         }
 
-        const verifiedToken = verifyToken(accessToken,envVars.JWT_ACCESS_SECRET) as JwtPayload
+        const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET) as JwtPayload
 
-        if(!authRoles.includes(verifiedToken.role)){
+        const isUserExist = await User.findOne({ email: verifiedToken.email })
+
+        if (!isUserExist) {
+            throw new AppError(httpStatus.BAD_REQUEST, "user does not exit")
+        }
+
+
+        if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
+            throw new AppError(httpStatus.BAD_REQUEST, `user is ${isUserExist.isActive}`)
+        }
+
+        if (isUserExist.isDelete) {
+            throw new AppError(httpStatus.BAD_REQUEST, "user is deleted")
+        }
+
+
+        if (!authRoles.includes(verifiedToken.role)) {
             throw new AppError(httpStatus.BAD_REQUEST, "you are not permitted to view this route")
         }
 
         req.user = verifiedToken
 
         next()
-        
+
     } catch (error) {
         next(error)
-        
+
     }
 }
--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,42 +1,87 @@
-import  bcryptjs  from 'bcryptjs';
-import  httpStatus  from 'http-status-codes';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import bcryptjs from 'bcryptjs';
+import httpStatus from 'http-status-codes';
 import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
-import { generateToken } from '../../utils/jwt';
+import { createNewAccessTokenWithRefreshToken, createUserToken } from '../../utils/userToken';
+import { JwtPayload } from 'jsonwebtoken';
 import { envVars } from '../../config/env';
 
-const credentialsLogin = async(payload :Partial<IUser>)=>{
-    const {email,password} = payload;
 
-    const isUserExist = await User.findOne({email})
 
-    if(!isUserExist){
+
+const credentialsLogin = async (payload: Partial<IUser>) => {
+    const { email, password } = payload;
+
+    const isUserExist = await User.findOne({ email })
+
+    if (!isUserExist) {
         throw new AppError(httpStatus.BAD_REQUEST, "email does not exit")
     }
 
     const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
 
-    if(!isPasswordMatched){
+    if (!isPasswordMatched) {
         throw new AppError(httpStatus.BAD_REQUEST, "incorrect password")
     }
 
-    const jwtPayload = {
-        userId : isUserExist._id,
-        email :  isUserExist.email,
-        role:isUserExist.role
-    }
+    const userTokens = createUserToken(isUserExist)
 
-    const accessToken = generateToken(jwtPayload,envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
 
-    return{
-        accessToken
+
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: pass, ...rest } = isUserExist.toObject()
+
+
+    return {
+        accessToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
+        user: rest
     }
 
 
 }
 
 
+
+
+const getNewAccessToken = async (refreshToken: string) => {
+
+    const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken)
+
+
+
+    return {
+        accessToken: newAccessToken
+    }
+
+
+}
+
+
+const resetPassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+
+    const user = await User.findById(decodedToken.userId)
+    const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user!.password as string)
+
+
+    if (!isOldPasswordMatch) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "old password does not matched")
+    }
+
+    user!.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPTJS_SALT_ROUND))
+
+    await user!.save()
+
+    
+
+}
+
+
 export const AuthServices = {
-    credentialsLogin
+    credentialsLogin,
+    getNewAccessToken,
+    resetPassword
 }
